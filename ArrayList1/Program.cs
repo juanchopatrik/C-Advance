9ebcad8766999d4d3b3681a56f9ccdfc2d1d5b24
juansilva <juandsr@hotmail.com>
2023-03-10 14:22:43 -0500

    Add project files.

---
+++ ArrayList1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList palabras = new ArrayList();
+            int n = 0;
+            int r = 0;
+
+            palabras.AddRange(new string[] { "hola", "como", "estan?" });
+
+            Console.WriteLine($"cantidad de elementos en palabras {palabras.Count}");
+
+            for (int i = 0; i < palabras.Count; i++)
+                Console.WriteLine($"{palabras[i]}");
+
+            Console.WriteLine("--------------");
+
+            palabras.Add("Bien y tu?");
+
+            foreach (string item in palabras)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            Console.WriteLine("--------------");
+
+            ArrayList valores = new ArrayList();
+            valores.Add(5);
+            valores.Add(7);
+            valores.Add(4);
+
+            for (int i = 0; i < valores.Count; i++)
+            {
+                r = (int)valores[i];
+                Console.WriteLine(r);
+            }
+
+            Console.WriteLine("--------------");
+
+            Console.WriteLine(valores.Contains(5));
+            Console.WriteLine(valores.Contains(17));
+
+            Console.WriteLine("--------------");
+
+            valores.Insert(2, 55);
+
+            for (int i = 0; i < valores.Count; i++)
+            {
+                r = (int)valores[i];
+                Console.WriteLine(r);
+            }
+
+            Console.WriteLine("--------------");
+
+            valores.Remove(55);
+
+            for (int i = 0; i < valores.Count; i++)
+            {
+                r = (int)valores[i];
+                Console.WriteLine(r);
+            }
+
+            Console.WriteLine("--------------");
+
+            valores.RemoveAt(0);
+
+            for (int i = 0; i < valores.Count; i++)
+            {
+                r = (int)valores[i];
+                Console.WriteLine(r);
+            }
+
+            Console.WriteLine("--------------");
+        }
+    }
+}
---
+++ BitArray1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable miTable = new Hashtable();
+
+            miTable.Add(123 , "Hola");
+            miTable.Add(234 , "Saludos");
+            miTable.Add(45 , "Adios");
+            miTable.Add(567 , "C#");
+            miTable.Add(12763 , "Programacion");
+
+            foreach (DictionaryEntry element in miTable)
+            {
+               Console.WriteLine($"{element.Key}, {element.Value}");
+            }
+
+            Console.WriteLine("---------------");
+            Console.WriteLine(miTable.Count);
+            Console.WriteLine("---------------");
+
+            Console.WriteLine(miTable[567]);
+            Console.WriteLine(miTable[445]);
+            Console.WriteLine("---------------");
+
+            miTable[45] = "Avanzado";
+            miTable[300] = "Colecciones";
+
+            foreach (DictionaryEntry element in miTable)
+            {
+               Console.WriteLine($"{element.Key}, {element.Value}");
+            }
+                
+            Console.WriteLine("---------------");
+            Console.WriteLine(miTable.Contains(567));
+            Console.WriteLine(miTable.Contains(335));
+            Console.WriteLine("---------------");
+
+            miTable.Remove(234);
+
+            foreach (DictionaryEntry element in miTable)
+            {
+               Console.WriteLine($"{element.Key}, {element.Value}");
+            }
+
+            Console.WriteLine("---------------");
+
+            foreach (int llave in miTable.Keys)
+            {
+                Console.WriteLine(llave);
+            }
+
+            Console.WriteLine("---------------");
+
+            foreach (int valor in miTable.Values)
+            {
+                Console.WriteLine(valor);
+            }
+
+        }
+    }
+}
